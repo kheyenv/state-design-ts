05ccb974c57e9dee23bf673d3cf1eb08f24220d3
kenneth heyen <kenneth.heyen@voltacharging.com>
2022-04-29 10:58:43 -0700

    I don't know what I'm doing...

--- index.ts
+++
@@ -1,4 +0,0 @@
-import Context from './Context'
-
-
-
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import Context from './Context'
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+    res.send('did a thing');
+})
+
+app.listen(port, () => {
+    console.log('[server]: Server is running at https://localhost:${port}');
+})
